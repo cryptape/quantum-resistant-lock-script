6844445c0957cfbdad31e940f662c9d52abcc969
Joii <joii@cryptape.com>
2023-02-23 18:21:34 +0800

    Adapt optimized code

--- tests/sphincsplus/optimization/optimization-sphincsplus.c
+++ tests/sphincsplus/optimization/optimization-sphincsplus.c
@@ -6,44 +6,22 @@
 #include <stdio.h>
 
 #include "ckb-sphincsplus.h"
-#include "ckb_vm_dbg.h"
 #include "test_data.h"
 
-#define RUN_SPHINCS_PLUS(NAME, SIZE, OPTION, THASH)           \
-  err = sphincs_plus_init_context(                            \
-      CRYPTO_TYPE_##NAME##_##SIZE##OPTION##_##THASH, &cctx);  \
-  if (err != 0) {                                             \
-    return 1;                                                 \
-  }                                                           \
-  err = sphincs_plus_verify(                                  \
-      &cctx, G_##NAME##_##SIZE##OPTION##_##THASH##_SIGN,      \
-      sizeof(G_##NAME##_##SIZE##OPTION##_##THASH##_SIGN),     \
-      G_##NAME##_##SIZE##OPTION##_##THASH##_MSG,              \
-      sizeof(G_##NAME##_##SIZE##OPTION##_##THASH##_MSG),      \
-      G_##NAME##_##SIZE##OPTION##_##THASH##_PUB_KEY,          \
-      sizeof(G_##NAME##_##SIZE##OPTION##_##THASH##_PUB_KEY)); \
-  if (err != 0) {                                             \
-    return 2;                                                 \
-  }
+#undef ASSERT
+// #define ASSERT(s)
+#define ASSERT(s) (void)0
+
+void randombytes(unsigned char *x, unsigned long long xlen) { ASSERT(0); }
 
 int main() {
-  crypto_context cctx = {0};
-  int err = 0;
-  // RUN_SPHINCS_PLUS(SHAKE, 128, S, ROBUST);
-  // RUN_SPHINCS_PLUS(SHAKE, 192, S, ROBUST);
-  // RUN_SPHINCS_PLUS(SHAKE, 256, S, ROBUST);
-
-  // RUN_SPHINCS_PLUS(SHAKE, 128, F, ROBUST);
-  // RUN_SPHINCS_PLUS(SHAKE, 192, F, ROBUST);
-  RUN_SPHINCS_PLUS(SHAKE, 256, F, ROBUST);
-
-  // RUN_SPHINCS_PLUS(SHAKE, 128, S, SIMPLE);
-  // RUN_SPHINCS_PLUS(SHAKE, 192, S, SIMPLE);
-  // RUN_SPHINCS_PLUS(SHAKE, 256, S, SIMPLE);
-
-  // RUN_SPHINCS_PLUS(SHAKE, 128, F, SIMPLE);
-  // RUN_SPHINCS_PLUS(SHAKE, 192, F, SIMPLE);
-  // RUN_SPHINCS_PLUS(SHAKE, 256, F, SIMPLE);
+  int err =
+      sphincs_plus_verify(G_TEST_DATA_SIGN, sizeof(G_TEST_DATA_SIGN),
+                          G_TEST_DATA_MSG, sizeof(G_TEST_DATA_MSG),
+                          G_TEST_DATA_PUB_KEY, sizeof(G_TEST_DATA_PUB_KEY));
+  if (err != 0) {
+    return 2;
+  }
 
   printf("Done");
   return 0;
